5c92a7d49c3a7e68a7bd584cfd4d96af3121dee8
Tim Caswell <creationix@vercel.com>
2025-11-18 10:44:56 -0600

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 
 import "./globals.css";
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
