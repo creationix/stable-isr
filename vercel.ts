82587890d12e4fd67ab85f34c27c94dc930328e9
Tim Caswell <creationix@vercel.com>
2026-03-27 17:34:55 -0700

    try again

--- vercel.ts
+++ vercel.ts
@@ -1,21 +1,22 @@
 import { compile } from "./rex"
 
-const src = rex`
-  // This is rex code that will be compiled to rexc bytecode
-  // and interpreted in proxy routing.
-  // We can set a custom response header
-  res.headers.x-flow-26 = "Hack the planet with style!"
+export default {
+  routes: [
+    rex`
+      // This is rex code that will be compiled to rexc bytecode
+      // and interpreted in proxy routing.
+      // We can set a custom response header
+      res.headers.x-flow-26 = "Hack the planet with style!"
 
-  // We can do arbitrary routing logic
-  when res.path == "/hello" do
-    name = req.query.name or "world"
-    res.headers.x-greeting = "Hwllo, " + name + "!"
-  end
-`
-
-
-export default { routes: { src } }
+      // We can do arbitrary routing logic
+      when res.path == "/hello" do
+        name = req.query.name or "world"
+        res.headers.x-greeting = "Hwllo, " + name + "!"
+      end
+    `        
+  ]
+}
 
 function rex([string]: TemplateStringsArray) {
-  return `rex:${compile(string.trim())}`
+  return { src: `^rex:${compile(string.trim())}$` }
 }
